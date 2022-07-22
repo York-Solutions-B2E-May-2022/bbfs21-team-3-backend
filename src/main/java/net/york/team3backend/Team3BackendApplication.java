4acd3bf31107bff88e32ed718d36a161d609a1bd
yorkmacbook060 <sadca_r@yahoo.com.ph>
2022-07-22 10:41:49 -0500

    first

---
+++ src/main/java/net/york/team3backend/Team3BackendApplication.java
@@ -0,0 +1,13 @@
+package net.york.team3backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team3BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team3BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/york/team3backend/Team3BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.york.team3backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team3BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
