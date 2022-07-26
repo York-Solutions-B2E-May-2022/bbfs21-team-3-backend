6846f77f61bc79388cba725dd411917f66026fbd
saad <osman>
2022-07-26 09:20:29 -0500

    Cookie Login 95%Done

--- src/main/java/net/york/team3backend/Entry/UserEntityController.java
+++ src/main/java/net/york/team3backend/Entry/UserEntityController.java
@@ -3,6 +3,10 @@ package net.york.team3backend.Entry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 @CrossOrigin
 public class UserEntityController {
@@ -15,11 +19,17 @@ public class UserEntityController {
 
     @PostMapping("/register")
     public void register(@RequestBody UserEntity user){
-        userEntityService.register(user);
+         userEntityService.register(user);
     }
 
     @GetMapping("/login")
     public UserEntity login(@RequestParam String username, @RequestParam String password){
         return userEntityService.login(username, password);
+    }
+    //Cookie Login Please Keep
+    @GetMapping("/")
+    public String getCookie(@CookieValue(name = "user-id") String userId) {
+        System.out.println(userId);
+            return userId;
     }
 }
--- src/main/java/net/york/team3backend/Entry/UserEntityService.java
+++ src/main/java/net/york/team3backend/Entry/UserEntityService.java
@@ -2,19 +2,30 @@ package net.york.team3backend.Entry;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class UserEntityService {
     UserEntityRepository userEntityRepository;
+    HttpServletResponse response;
+    private HashMap<UUID, Long> tokenMap;
+
+
 
     @Autowired
-    public UserEntityService(UserEntityRepository userEntityRepository) {
+    public UserEntityService(UserEntityRepository userEntityRepository, HttpServletResponse response) {
         this.userEntityRepository = userEntityRepository;
+        this.tokenMap = new HashMap<>();
+        this.response = response;
     }
 
     public void register(UserEntity userEntity){
@@ -27,6 +38,35 @@ public class UserEntityService {
             newUser.setEmail(userEntity.getEmail());
             newUser.setRole(userEntity.getRole());
             userEntityRepository.save(newUser);
+            //Cookie Logic Please Keep
+            UUID token = UUID.randomUUID();
+            tokenMap.put(token, userEntity.getId());
+
+            Cookie jwtTokenCookie = new Cookie("user-id", token.toString());
+            jwtTokenCookie.setMaxAge(86400);
+            jwtTokenCookie.setSecure(false);
+            jwtTokenCookie.setHttpOnly(false);
+            jwtTokenCookie.setPath("/");
+            jwtTokenCookie.setDomain("localhost");
+            response.addCookie(jwtTokenCookie);
+
+            //NOTES
+//            The request cookie is what is send from the client to the server
+//            (thus what the browser provides). The response cookie are the cookies
+//            that you want to place in the browser.The next connection from the browser
+//            that accepted the cookie from the response object will provide the cookie in
+//            the request object.
+
+
+//            ResponseCookie resCookie = ResponseCookie.from("token", token.toString())
+//                    .httpOnly(false)
+////                    .sameSite("None")
+//                    .secure(false)
+//                    .path("/")
+//                    .maxAge(Math.toIntExact(86400))
+//                    .build();
+//            response.addHeader("Set-Cookie", resCookie.toString());
+
         }
     }
 
